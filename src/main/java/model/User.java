064a2f98bf8ae7db74563a574a21df9cea534d0c
nodlami022 <nodlami022@student.wethinkcode.co.za>
2023-08-30 14:30:54 +0200

    changes

---
+++ src/main/java/model/User.java
@@ -0,0 +1,53 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "UserPos")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "type_user")
+    private String type;
+
+    @Column(name = "user_password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/test/java/repository/DepartmentRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DepartmentRepositoryTest {
-
-}
