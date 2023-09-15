738d735748160250d208a0f76d6c1e56d1e033f2
nodlami022 <nodlami022@student.wethinkcode.co.za>
2023-09-15 12:50:42 +0200

    Updated Home Page

--- src/main/java/org/pos/controller/HomeController.java
+++ src/main/java/org/pos/controller/HomeController.java
@@ -1,6 +1,8 @@
 package org.pos.controller;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -12,71 +14,20 @@ public class HomeController {
 
 
     public void cashiers(ActionEvent event){
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-
-        try {
-//            Parent root_ = FXMLLoader.load(getClass().getResource("Home.fxml"));
-
-            //Used for testing
-            Pane root = new Pane(); //
-
-            Text text = new Text(); //
-            text.setFill(Color.BLACK); //
-            text.setText("THis is the cashiers scene"); //
-            text.setX(100); //
-            text.setY(100); //
-            root.getChildren().add(text); //
-
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Loads cash scene");
 
     }
 
 
     public void sales(ActionEvent event){
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-
-        try {
-//            Parent root_ = FXMLLoader.load(getClass().getResource("Home.fxml"));
-
-            //Used for testing
-            Pane root = new Pane(); //
-
-            Text text = new Text(); //
-            text.setFill(Color.BLACK); //
-            text.setText("THis is the sales scene"); //
-            text.setX(100); //
-            text.setY(100); //
-            root.getChildren().add(text); //
-
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    };
+        System.out.println("Loads sales scene");
+    }
 
     public void department(ActionEvent event){
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-
+        
         try {
-//            Parent root_ = FXMLLoader.load(getClass().getResource("Home.fxml"));
-
-            //Used for testing
-            Pane root = new Pane(); //
-
-            Text text = new Text(); //
-            text.setFill(Color.BLACK); //
-            text.setText("This is the department scene"); //
-            text.setX(100); //
-            text.setY(100); //
-            root.getChildren().add(text); //
-
+            Parent root = FXMLLoader.load(getClass().getResource("Department.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
