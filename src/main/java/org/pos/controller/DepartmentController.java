4c9337987331bbce4fd3efccfc280b0bc8645658
Nombuso Dlamini <101812346+Nombuso16@users.noreply.github.com>
2023-09-15 13:02:11 +0200

    adding the Home Phase (#8)
    * adding the Home Phase
    
    * Updated Home Page
    
    * Edited the Home Page
    
    * Changes on the Home Page
    
    ---------
    
    Co-authored-by: nodlami022 <nodlami022@student.wethinkcode.co.za>

---
+++ src/main/java/org/pos/controller/DepartmentController.java
@@ -0,0 +1,4 @@
+package org.pos.controller;
+
+public class DepartmentController {
+}
---
+++ src/main/java/org/pos/controller/HomeController.java
@@ -0,0 +1,36 @@
+package org.pos.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class HomeController {
+
+    public void cashiers(ActionEvent event){
+        System.out.println("Loads cash scene");
+
+    }
+
+    public void sales(ActionEvent event){
+        System.out.println("Loads sales scene");
+    }
+
+    public void department(ActionEvent event){
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Department.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
+}
